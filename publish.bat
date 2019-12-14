e6132b0a30d2cb2a4659624044967f5957ba475c
ming <3133309264@qq.com>
2019-12-15 04:51:16 +0800

    修改

--- publish.bat
+++ publish.bat
@@ -1,5 +1,2 @@
-npm run build
 
-call pscp.exe -pw 123456dlM -r ./dist/count/* root@111.229.157.73:/data/web2
-
-REM call pscp.exe -pw 123456dlM -r ./dist/count/* root@111.229.157.73:/data/web3
+call pscp.exe -pw 123456dlM -r ./index.html root@111.229.157.73:/data/web1
